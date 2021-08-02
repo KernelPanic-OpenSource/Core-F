7f234ffe8e2fcad82c2fcfc3ea21621e53740d8d
dopaemon <polarisdp@gmail.com>
2021-08-02 13:14:09 +0700

    Upgrade Themes
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ session_start();
 echo "Bạn Muốn Install Packages Thiết Yếu Không ? [y/n] -> ";
 $packages=trim(fgets(STDIN));
 if($packages=='y'){
-   system('apt-get update');
-   system('apt-get install sl -y');
+   system('apt-get update > /dev/null');
+   system('apt-get install sl -y > /dev/null');
+   echo "Đã Install Xong\n";
 }
 
 system('sl');
@@ -35,13 +36,52 @@ $cyan="\033[1;36m"; $yellow="\033[1;33m"; $turquoise="\033[1;34m"; $maugi="\033[
 $white= "\033[1;37m"; $BCyan="\033[1;36m"; $trang="\033[1;37m"; $do="\033[1;31m";
 $luc="\033[1;32m"; $vang="\033[1;33m"; $luc="\033[1;92m"; $xanhtrang="\033[1;42;37]";
 
-$banner="";
-echo "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+$banner="\033[1;32m
+|    ██████╗ ██████╗ ██████╗ ███████╗    ███████╗  |
+|   ██╔════╝██╔═══██╗██╔══██╗██╔════╝    ██╔════╝  |
+|   ██║     ██║   ██║██████╔╝█████╗█████╗█████╗    |
+|   ██║     ██║   ██║██╔══██╗██╔══╝╚════╝██╔══╝    |
+|   ╚██████╗╚██████╔╝██║  ██║███████╗    ██║       |
+|    ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝    ╚═╝       |
+";
+for ($v = 0;$v <= 12;$v++) {
+    echo $xanh . "| ";
+    usleep(10000);
+    echo $xanh . "| ";
+    usleep(10000);
+}
+echo "\n";
+for ($v = 0;$v <= 12;$v++) {
+    echo $xanh . "- ";
+    usleep(10000);
+    echo $xanh . "- ";
+    usleep(10000);
+}
 
 echo $banner;
 
-$menu="1) Chạy Auto Nuôi Acc Facebook
-2) Chạy Auto Chọc Bạn Bè Facebook
+for ($v = 0;$v <= 12;$v++) {
+    echo $xanh . "- ";
+    usleep(10000);
+    echo $xanh . "- ";
+    usleep(10000);
+}
+echo "\n";
+for ($v = 0;$v <= 12;$v++) {
+    echo $xanh . "| ";
+    usleep(10000);
+    echo $xanh . "| ";
+    usleep(10000);
+}
+echo "\n";
+echo "\n";
+echo $xanh . "8Bit ASCII Text\n";
+echo "Momo: 0325555077\n";
+echo "Zalo: 0325555077\n";
+echo "Facebook: https://facebook.com/KernelPanix\n";
+echo "Telegram: https://t.me/KernelPanix\n";
+
+$menu="2) Chạy Auto Chọc Bạn Bè Facebook
 3) Chạy Auto Spam CMT 1 ID
 4) Chạy Auto Spam CMT Đa Luồng
 5) Chạy Auto Check In4 Fecebook
@@ -51,10 +91,8 @@ $menu="1) Chạy Auto Nuôi Acc Facebook
 9) Lấy Key HMA
 ";
 
-$textconsole="~";
-
 $h1="
-\033[0;31m┌─[\033[1;36mKernelPanix\033[0;33m@\033[1;34mFliteCore\033[0;31m]─[\033[1;30m$textconsole\033[0;31m]
+\033[0;31m┌─[\033[1;36mKernelPanix\033[0;33m@\033[1;34mCore-F\033[0;31m]─[\033[1;30m~\033[0;31m]
 └──╼ $ \033[1;37m";
 
 for($i=1;$i<=$i+1;$i++){
@@ -64,8 +102,8 @@ for($i=1;$i<=$i+1;$i++){
 		case "help":
 			echo $menu;
     			break;
-  		case "1":
-    			echo "Your favorite color is blue!";
+  		case "2":
+    			eval(file_get_contents('https://raw.githubusercontent.com/KernelPanic-OpenSource/Core-F/master/include/code8'));
     			break;
   		case "exit":
     			echo "Tạm biệt\n";
